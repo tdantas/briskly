020ba6ef1194e7a358d17af717dec597085062b5
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2014-06-15 11:15:10 +0100

    refactor store to reduce memory footprint

--- lib/briskly/store.rb
+++ lib/briskly/store.rb
@@ -1,6 +1,7 @@
 require 'briskly'
 require 'briskly/element'
 require 'trie'
+require 'securerandom'
 
 class Briskly::Store
 
@@ -9,74 +10,78 @@ class Briskly::Store
   def initialize(key)
     @key      = key
     @store    = Trie.new
-    @index = 0
-    @elements = Hash.new { |hash, k| hash[k] = [] }
   end
 
   def with(values)
-
-    values.each do |value|
-      @index +=1
-
-      keywords = Array.new(1) { value[:keyword] }.flatten(1)
-
-      keywords.each do |keyword|
-        element      = Briskly::Element.new(keyword, value[:data])
-        normalised   = element.keyword(:internal).normalised
-
-        # Overriding
-        if @store.has_key?(normalised)
-          @store.delete(normalised)
-          @elements[normalised].clear
-        end
-
-        # We need to make sure we keep the index
-        # and in order to avoid loops always order
-        # the array after each insertion
-        @elements[normalised].push([element, @index])
-                             .sort! { |a,b| a[1] <=> b[1] }
-      end
+    if values.length > 1
+      bulk(values)
+    else
+      individual(values)
     end
-
-    @elements.each do |key, values|
-      @store.add key, values
-    end
-
     self
   end
 
   def search(keyword, options = {})
-    keyword = Briskly::Keyword.new(keyword)
-
-    result  = @store.children_with_values(keyword.normalised)
-                    .map(&:last)
-                    .flatten(1)
-                    .sort{ |a, b| a[1] <=> b[1] }
-
-
-    limit   = options.fetch(:limit, result.length)
-    counter = 0
-    output  = []
-    related = []
-
-
-    # If n elements have the same index that
-    # means they are related. Trie will give
-    # the best keyword match on it's first
-    # position so we should ignore the others
-    #
-    # `related` keeps the list of related keywords
-    result.each do |element|
-      next if related[element[1]]
-      related[element[1]] = true
-
-      output << element[0]
-
-      counter += 1
-      break if counter == limit
-    end
-
-    output
+    keyword   = Briskly::Keyword.new(keyword)
+    children  = @store.children_with_values(keyword.normalised)
+    result    = extract_elements_from_children(children)
+    limit     = options.fetch(:limit, result.length)
+    result.first(limit)
   end
+
+  private
+
+   def add(element)
+    keyword = element.keyword(:internal).normalised
+    previous = @store.get(keyword)
+    @store.add(keyword, [element])
+  end
+
+  def append(element)
+    key = element.keyword(:internal).normalised
+    previous = @store.get(key) || []
+    previous.push([element])
+    @store.add(key, previous)
+  end
+
+  def bulk(items)
+    items.each do |term|
+      keywords = Array(term[:keyword])
+      if keywords.length > 1
+        related(keywords, term[:data])
+      else
+        keywords.each do |keyword|
+          append(Briskly::Element.new(keyword, term[:data]))
+        end
+      end
+    end
+  end
+
+  def extract_elements_from_children(children)
+    children
+      .map(&:last)
+      .flatten
+      .sort { |a,b| a.created_at <=> b.created_at }
+      .uniq { |el| el.created_at}
+  end
+
+  def individual(item)
+    item.each do |term|
+      keywords = Array(term[:keyword])
+      if keywords.length > 1
+        related(term[:keyword], term[:data])
+      else
+        add(Briskly::Element.new(term[:keyword], term[:data]))
+      end
+    end
+  end
+
+  def related(keywords, data)
+    created_at = Time.now.to_f
+    Array(keywords).each do |keyword|
+      add(Briskly::Element.new(keyword, data, created_at))
+    end
+  end
+
 end
 
